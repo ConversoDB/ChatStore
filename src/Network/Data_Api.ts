6f0c044506d9b2a6de5d17b8e0daf9c6d31d69aa
Anwesha <anweshadas909@gmail.com>
2025-08-01 13:15:25 +0530

    initial commit

---
+++ src/Network/Data_Api.ts
@@ -0,0 +1,39 @@
+import type {wordsModel} from "../Models/wordsModel.ts";
+
+const api_route = "http://localhost:5000";
+
+export const getWords = async (email: string) => {
+    const response = await fetch(`${api_route}/api/words/${email}`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    return response.json();
+}
+
+export const editWords = async (word : wordsModel) => {
+    const response = await fetch(`${api_route}/api/words/${word._id}`, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(word)
+    })
+
+    return response.json();
+}
+
+export const deleteWords = async (word : wordsModel) => {
+    const response = await fetch(`${api_route}/api/words/${word._id}`, {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    const data = await response.json()
+
+    return data;
+}
